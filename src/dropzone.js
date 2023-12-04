b3fd5c4d4ffeba1816b4c0c008f0d46f7eef19f0
Nicolas CARPi <nico-git@deltablot.email>
2023-12-04 03:14:11 +0100

    start cleanup

--- src/dropzone.js
+++ src/dropzone.js
@@ -63,9 +63,9 @@ export default class Dropzone extends Emitter {
       this.element = document.querySelector(this.element);
     }
 
-    // Not checking if instance of HTMLElement or Element since IE9 is extremely weird.
-    if (!this.element || this.element.nodeType == null) {
-      throw new Error("Invalid dropzone element.");
+    // make sure we actually have an HTML Element
+    if (!this.element instanceof HTMLElement) {
+      throw new Error("Invalid dropzone element: not an instance of HTMLElement.");
     }
 
     if (this.element.dropzone) {
@@ -120,18 +120,6 @@ export default class Dropzone extends Emitter {
 
     if (this.options.binaryBody && this.options.uploadMultiple) {
       throw new Error("You cannot set both: binaryBody and uploadMultiple.");
-    }
-
-    // Backwards compatibility
-    if (this.options.acceptedMimeTypes) {
-      this.options.acceptedFiles = this.options.acceptedMimeTypes;
-      delete this.options.acceptedMimeTypes;
-    }
-
-    // Backwards compatibility
-    if (this.options.renameFilename != null) {
-      this.options.renameFile = (file) =>
-        this.options.renameFilename.call(this, file.name, file);
     }
 
     if (typeof this.options.method === "string") {
@@ -2244,65 +2232,6 @@ class ExifRestore {
   }
 }
 ExifRestore.initClass();
-
-/*
- * contentloaded.js
- *
- * Author: Diego Perini (diego.perini at gmail.com)
- * Summary: cross-browser wrapper for DOMContentLoaded
- * Updated: 20101020
- * License: MIT
- * Version: 1.2
- *
- * URL:
- * http://javascript.nwbox.com/ContentLoaded/
- * http://javascript.nwbox.com/ContentLoaded/MIT-LICENSE
- */
-
-// @win window reference
-// @fn function reference
-let contentLoaded = function (win, fn) {
-  let done = false;
-  let top = true;
-  let doc = win.document;
-  let root = doc.documentElement;
-  let add = doc.addEventListener ? "addEventListener" : "attachEvent";
-  let rem = doc.addEventListener ? "removeEventListener" : "detachEvent";
-  let pre = doc.addEventListener ? "" : "on";
-  var init = function (e) {
-    if (e.type === "readystatechange" && doc.readyState !== "complete") {
-      return;
-    }
-    (e.type === "load" ? win : doc)[rem](pre + e.type, init, false);
-    if (!done && (done = true)) {
-      return fn.call(win, e.type || e);
-    }
-  };
-
-  var poll = function () {
-    try {
-      root.doScroll("left");
-    } catch (e) {
-      setTimeout(poll, 50);
-      return;
-    }
-    return init("poll");
-  };
-
-  if (doc.readyState !== "complete") {
-    if (doc.createEventObject && root.doScroll) {
-      try {
-        top = !win.frameElement;
-      } catch (error) {}
-      if (top) {
-        poll();
-      }
-    }
-    doc[add](pre + "DOMContentLoaded", init, false);
-    doc[add](pre + "readystatechange", init, false);
-    return win[add](pre + "load", init, false);
-  }
-};
 
 function __guard__(value, transform) {
   return typeof value !== "undefined" && value !== null
